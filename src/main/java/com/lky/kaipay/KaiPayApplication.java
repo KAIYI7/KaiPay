7dc0dab0977cc2da1fc0de4377ab790084db7d5b
Kai Yi <kaiyi.lee@alphawave-innovations.com>
2026-08-21 09:32:17 +0800

    Initial commit

---
+++ src/main/java/com/lky/kaipay/KaiPayApplication.java
@@ -0,0 +1,13 @@
+package com.lky.kaipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaiPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaiPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lky/kaipay/KaiPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lky.kaipay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaiPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
